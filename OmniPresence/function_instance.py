ed885d4be173f20b1eaba642c76ddaa84411c2c0
MateusMoutinho <mateusmoutinho01@gmail.com>
2022-07-15 10:13:01 -0300

    starting defining multprocess

--- OmniPresence/function_instance.py
+++ OmniPresence/function_instance.py
@@ -1,4 +1,6 @@
 
+from inspect import ArgSpec
+from multiprocessing import Process, Queue
 
 from typing import Callable
 
@@ -10,11 +12,24 @@ class FunctionInstance:
         self._runing = False 
         self._args = args
         self._kwargs = kwargs
+        self._result = None
         self._execute_oldest_instance:Callable = None 
 
     def _execute(self):
+        queue = Queue()
+        def target(queue:Queue):
+            try:
+                result = self._function(*self._args,**self._kwargs)
+                queue.put(result)
+            except Exception as e:
+                pass
+
+        p = Process(target=target,args=[queue])
+        p.start()
+        p.join() # this blocks until the process terminates
+        self._result = queue.get()
+        print(self._result)
         
-        result = self._function(*self._args,**self._kwargs)
         self._execute_oldest_instance()
 
     def then(self):
