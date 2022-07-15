76c1c33a32086bfa2f8857f1e579e566fa47fea4
MateusMoutinho <mateusmoutinho01@gmail.com>
2022-07-15 13:45:28 -0300

    paralelizador iniciado

---
+++ QuantumPresence/processor.py
@@ -0,0 +1,27 @@
+from multiprocessing import Process, Manager
+from typing import List
+
+from QuantumPresence.function_instance import FunctionInstance
+
+class Processor:
+    pass 
+
+    def __init__(self) -> None:
+        self._instances:List[FunctionInstance] = []
+
+    def total_instances(self):
+        return len(self._instances)
+
+    def add_instance(self,instance:FunctionInstance):
+        self._instances.append(instance)
+
+
+    def run_all_instances(self):
+        def target(instances:List[FunctionInstance]):
+            for instance in instances:
+                instance._execute()
+               
+
+        p = Process(target=lambda:target(self._instances))
+        
+        p.start()
