c965b04763dbb3045dd4147ce635f9e52ecb279a
MateusMoutinho <mateusmoutinho01@gmail.com>
2022-07-15 11:53:20 -0300

    creating event

--- QuantumPresence/function_instance.py
+++ QuantumPresence/function_instance.py
@@ -1,6 +1,6 @@
 
 from inspect import ArgSpec
-from multiprocessing import Process, Queue
+from multiprocessing import Process, Manager
 
 from typing import Callable
 
@@ -10,31 +10,42 @@ class FunctionInstance:
     def __init__(self,function:Callable,args=[],kwargs={}) -> None:
         self._function = function
         self._status = 'uninitialized'
+        
         self._args = args
         self._kwargs = kwargs
-        self._result = None
+        self._event = None
 
     def _execute(self):
-        self._queue = Queue()
         self._status = 'running'
-        def target(queue:Queue):
+        self._event = Manager().dict()
+        def target(event:dict):
+            event['function_name'] =self._function.__name__
+            event['function'] = self._function
+            event['args'] = self._args
+            event['kwargs'] = self._kwargs
+            event['error'] = None 
+            event['return'] = None 
+ 
             try:
-                result = self._function(*self._args,**self._kwargs)
-                queue.put(result)
+                return_value = self._function(*self._args,**self._kwargs)
+                event['return'] = return_value
             except Exception as e:
-                pass
-
-        self._process  = Process(target=target,args=[self._queue])
+                event['error'] = e
+                
+        self._process  = Process(target=target,args=[self._event])
         self._process.start()
     
+
 
     def _update_satus(self)->str:
         if not self._process.is_alive():
             self._status = 'executed'
+            self._process.join()
+            
         return self._status
 
-    def then(self):
+
+    def then(self,function:Callable):
         pass 
 
-    def catch(self):
-        pass 
+
--- teste.py
+++ teste.py
@@ -1,5 +1,5 @@
 from time import sleep
-from QuantumPresence.parallelizer import Parallelizer
+from QuantumPresence import Parallelizer
 
 
 
@@ -7,11 +7,13 @@ def soma(x,y):
     sleep(1)
     result =  x + y 
     print('o resultado e',result)
-
+    return result
 
 p = Parallelizer(instances=10)
-for x in range(0,60):
+for x in range(0,20):
     p.add_function(soma,[x,10])
     
 
 p.start_main_loop()
+for x in p._instances:
+    print(x._event)
